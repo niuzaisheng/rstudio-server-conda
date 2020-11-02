93ad65276baac6337a8616a3970d3a246999b55c
niu_server15 <niuniu_shan@outlook.com>
2020-11-02 13:33:56 +0800

    添加自动重启代码

---
+++ after_reboot_create_rstudio_tmp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir /tmp/rstudio-rsession
+sudo chown rstudio-server:rstudio-server /tmp/rstudio-rsession
+mkdir /tmp/rstudio-server
+sudo chown rstudio-server:rstudio-server /tmp/rstudio-server
